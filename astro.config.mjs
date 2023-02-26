67f6b72a7a537e6469a3fc389f66494f5e37c2fd
Abdulrahman Falyoun <falyoun.abdulrahman@gmail.com>
2023-02-26 14:12:29 +0400

    [Add]: base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
+console.log({ env: process.env.NODE_ENV })
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
