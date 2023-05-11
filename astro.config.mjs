3145d9f656546548460cc5a87ff50dc7f331ccaf
KhoiUna <thorwaitson@gmail.com>
2023-05-11 02:05:00 -0500

    add astro + react integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 // https://astro.build/config
+import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
-console.log({ env: process.env.NODE_ENV })
+console.log({ env: process.env.NODE_ENV });
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  base: '/astro-portfolio',
+  integrations: [tailwind(), react()],
+  base: "/astro-portfolio",
 });
